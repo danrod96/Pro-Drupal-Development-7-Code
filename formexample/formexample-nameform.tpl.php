bcb8ec3ed31809ac6dc6e98ea0a89a8799f02f5f
EC2 Default User <danrod@gmail.com>
2016-12-12 15:32:45 +0000

    New modules

---
+++ formexample/formexample-nameform.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ *
+ * This is the template file for rendering the formexample nameform.
+ * In this file each element of the form is rendered individually
+ * instead of the entire form at once, giving me the ultimate control
+ * over how my forms are laid out. I could also print the whole form
+ * at once - using the predefined layout in the module by
+ * printing $variables['formexample_nameform_form'];
+ *
+ */
+ print '<div id="formexample_nameform_form">';
+ print $variables['formexample_nameform']['color'];
+ print $variables['formexample_nameform']['name'];
+ print $variables['formexample_nameform']['country'];
+ print $variables['formexample_nameform']['flavor'];
+ print $variables['formexample_nameform']['submit'];
+ print $variables['formexample_nameform']['hidden'];
+ print '</div>';
+
+ dpm($variables['formexample_nameform']['color']);
+
+ //print $variables['formexample_nameform_form'];
+?>
